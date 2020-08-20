4f1e1cc11a7bb6260fa8e7b7de7b0b45bf3733dc
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2020-08-20 10:55:42 +0200

    Changed Title Name To Photo-Share

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    PhotoShare
                 </div>
 
                 <div class="links">
