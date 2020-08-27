43acf1f1e79a56a64a90695cd40a3fb36f418ff0
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2020-08-27 13:43:39 +0200

    Update index.blade.php

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,24 +4,28 @@
 <div class="container">
      <div class="row">
          <div class="col-3 p-5">
-            <img height ="80" src="https://s3.amazonaws.com/freecodecamp/curriculum-diagram-full.jpg" class = "rounded-circle " >
+            <img src="{{$user->profile->profileImage()}}" class="rounded-circle w-100" >
          </div>
-         <div class ="col-9 pt-5">  
+         <div class="col-9 pt-5">  
              <div class ="d-flex justify-content-between align-items-baseline"> 
-                    <h1>{{$user->username}}</h1> 
+                   <div class="d-flex align-items-center pb-3" >
+                           <div class="h4">{{$user->username}}</div> 
+
+                           <follow-button></follow-button>
+                    </div>
                     @can('update',$user->profile)
-                   <a href ="/p/create">Add New Post</a> 
+                   <a href="/p/create">Add New Post</a> 
                     @endcan
              </div>
              @can('update',$user->profile)
-                   <a href ="/profile/{{$user->id}}/edit">Edit Profile</a> 
+                   <a href="/profile/{{$user->id}}/edit">Edit Profile</a> 
                     @endcan
-             <div class = "d-flex" >
+             <div class="d-flex" >
                  <div class="pr-3"><strong>{{$user->posts->count()}}</strong>Posts</div>
                  <div class="pr-3"><strong>10</strong>Followers</div>
                  <div class="pr-3"><strong>500k</strong>Following</div>
             </div>
-            <div class ="pt-4 font-weight-bold">{{$user->profile->title}}</div>
+            <div class="pt-4 font-weight-bold">{{$user->profile->title}}</div>
             <div> {{$user->profile->description}}</strong> </div>
             <div> <a href="#">{{$user->profile->url ??'N/A'}} </a> </div>
         </div> 
