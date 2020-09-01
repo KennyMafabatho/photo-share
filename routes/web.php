bd6ba2d9bf57cde41b0f960e144df6105802b343
Kenny Mafabatho <mafabatholehlohonolo@gmail.com>
2020-09-01 12:23:57 +0200

    Changed login redirect path. Renamed welcome page title and changed backround.

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,15 @@ use Illuminate\Support\Facades\Route;
 
  
 Auth::routes();
-
+Route::get('/welcome','HomeController@index');
 Route::post('/follow/{user}','FollowsController@store');
 Route::get('/','PostsController@index');
 Route::get('/p/create', 'PostsController@create');
 Route::get('/p/{post}', 'PostsController@show');
 Route::post('/p', 'PostsController@store');
+Route::get('/profile', function(){
+    return redirect('/profile/'.auth()->user()->id);
+});
 Route::get('/profile/{user}', 'ProfilesController@index')->name('profile.show');
 Route::get('/profile/{user}/edit', 'ProfilesController@edit')->name('profile.edit');
 Route::patch('/profile/{user}', 'ProfilesController@update')->name('profile.update');
